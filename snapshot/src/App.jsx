c8f983e2da777666f64aafb6251b9a51bbc70279
RoboticCat7000 <rajathsukeertib@gmail.com>
2023-04-07 16:45:14 +0530

    first commit

---
+++ snapshot/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import ImagesSection from "./components/ImagesSection";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>Snap Shot!</h1>
+      </header>
+      <ImagesSection />
+    </div>
+  );
+}
+
+export default App;
